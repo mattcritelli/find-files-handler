f83cfb75776c26a15fe17bc512c132c778e9f258
Matt C <matthew.critelli@gmail.com>
2018-10-12 13:09:27 -0600

    Before refactor

---
+++ findFiles.js
@@ -0,0 +1,167 @@
+// Use the below to automatically create the list of array.
+// Must be run in node, will not work in browser
+const fs = require('fs');
+const util = require('util')
+const multiConditionHandler = require('./multiConditionHandler');
+
+
+function findFileNames(dirName){
+  // directory below may need to be modified slightly depending on folder structure
+  return fs.readdirSync(`${dirName}`)
+           .map(filename => filename.replace(/-01.svg/, '').toLowerCase());
+}
+
+/* Automatically create custom rule objects */
+function createCustomRules(optionsArray, floorNum){
+  const conditionals = [' and not ', ' and ', ' join ', ' or '] // 'and not' must come before 'and' to properly sort
+  const primaryOutput = [];
+  const joinRules = [];
+  const multiRules = []
+
+  optionsArray.forEach(option => {
+    let count = multiConditionHandler.conditionalCount(option)
+
+    if(count === 9){
+      console.log('\noption count 1:', option)
+      if(option.includes(' and not ')){
+        const optArr = option.split(' and not ')
+        primaryOutput.push({
+          hrefs: option.split(' and not '),
+          altHref: option,
+          replaceHref: optArr[0],
+          floor: floorNum,
+          ruleType: 'andNot'
+        })
+      } else if(option.includes(' and ')){
+        const optArr = option.split(' and ')
+        primaryOutput.push({
+          hrefs: optArr,
+          altHref: option,
+          replaceHref: optArr[0],
+          floor: floorNum,
+          ruleType: 'and'
+        })
+      } else if(option.includes(' or ')){
+        primaryOutput.push({
+          hrefs: option.split(' or '),
+          altHref: option,
+          floor: floorNum,
+          ruleType: 'or'
+        })
+      } else if(option.includes(' join ')){
+        joinRules.push({
+          hrefs: option.split(' join '),
+          altHref: option,
+          floor: floorNum,
+          ruleType: 'join'
+        })
+      }
+    } else if (count >= 1){
+      console.log('\noption count 2+:', option)
+      const conditionalIndices = multiConditionHandler.findIndexOfEachConditional(option)
+      const sortedConditionals = multiConditionHandler.sortMultiConditionIntoArrays(conditionalIndices, option)
+      const ruleToAdd = multiConditionHandler.formatMultiCustomRule(sortedConditionals, floorNum, option)
+      multiRules.push(ruleToAdd)
+    }
+  })
+  return primaryOutput.concat(joinRules).concat(multiRules)
+}
+
+/* Create non-duplicate list of all possible options */
+function sanitizeWhitelist(customRules, initialList){
+  initialList = initialList.filter(opt => opt !== '.ds_store')
+  const aggregateOptions = []
+  const output = []
+
+  customRules.forEach(rule => {
+    // remove all conditional filenames from initial list
+    // i.e. 'dvgslstcorn or woodlstcorn or eleclstcorn' would be removed
+    const optIndex = initialList.indexOf(rule.altHref)
+    if(optIndex >= 0){
+      initialList.splice(optIndex, 1)
+    }
+
+    if(rule.ruleType !== 'multi'){
+      // add individual options in conditional filename to aggregateOptions
+      // i.e. [ 'dvgslstcorn', 'woodlstcorn', 'eleclstcorn' ]
+      rule.hrefs.forEach(opt => aggregateOptions.push(opt))
+    } else {
+      Object.keys(rule.multiGroup).forEach(group => {
+        rule.multiGroup[group].forEach(opt => aggregateOptions.push(opt))
+      })
+    }
+  })
+
+  // Remove all duplicates by checking output array before adding
+  aggregateOptions.forEach(opt => {
+    if(!output.includes(opt)){
+      output.push(opt)
+    }
+  })
+  return output.concat(initialList)
+}
+
+//////////////////////////////////////////////////////////////////////
+//                                                                  //
+// FOR NOW - NEED TO MANUALLY ADJUST BELOW BASED ON MODEL SPECIFICS //
+//                                                                  //
+//////////////////////////////////////////////////////////////////////
+
+// OAKWOOD DIRECTORIES
+// const directory = 'Oakwood/167_2322v2'
+// const directory = 'Oakwood/167_2323/167_2323v2Rev'
+
+
+// ARBOR HOME DIRECTORIES
+const directory = 'Arbor/Bradford(868)/199_868'
+// const directory = 'Arbor/Chestnut(869)/199_869'
+// const directory = 'Arbor/199_864-Cottonwood'
+// const directory = 'Arbor/199_872-Aspen/199_872'
+// const directory = 'Arbor/Magnolia(880)'
+// const directory = 'Arbor/Cooper(7448)'
+// const directory = 'Arbor/Mulberry(874)'
+// const directory = 'Arbor/Empress(877)'
+
+
+/* Extract all file names from individual floor option folders */
+
+// let floor_0 = findFileNames(`../${directory}/floor_0`)
+// console.log('floor_0 options:', floor_0)
+//
+let floor_1 = findFileNames(`../${directory}/floor_1`)
+console.log('floor_1 options:', floor_1)
+// //
+let floor_2 = findFileNames(`../${directory}/floor_2`)
+console.log('floor_2 options:', floor_2)
+//
+// let floor_3 = findFileNames(`../${directory}/floor_3`)
+// console.log('floor_3 options:', floor_3)
+
+/* Automatically create custom rule objects */
+// const customRulesFloorZero = createCustomRules(floor_0, 0)
+const customRulesFloorOne = createCustomRules(floor_1, 1)
+const customRulesFloorTwo = createCustomRules(floor_2, 2)
+// const customRulesFloorThree = createCustomRules(floor_3, 3)
+
+
+// console.log('customRulesFloorZero:', customRulesFloorZero)
+// console.log('customRulesFloorOne:', customRulesFloorOne)
+// console.log('customRulesFloorTwo:', customRulesFloorTwo)
+// console.log('customRulesFloorThree:', customRulesFloorThree)
+// console.log('all Custom Rules:', util.inspect(customRulesFloorOne, {showHidden: false, depth: null}))
+// console.log('all Custom Rules:', util.inspect(customRulesFloorZero.concat(customRulesFloorOne).concat(customRulesFloorTwo), {showHidden: false, depth: null}))
+console.log('all Custom Rules:', util.inspect(customRulesFloorOne.concat(customRulesFloorTwo), {showHidden: false, depth: null}))
+
+
+/* Create non-duplicate list of all possible options */
+// const whitelistOutputFloorZero = sanitizeWhitelist(customRulesFloorZero, floor_0)
+const whitelistOutputFloorOne = sanitizeWhitelist(customRulesFloorOne, floor_1)
+const whitelistOutputFloorTwo = sanitizeWhitelist(customRulesFloorTwo, floor_2)
+// const whitelistOutputFloorThree = sanitizeWhitelist(customRulesFloorThree, floor_3)
+
+
+// console.log('WHITELISTS')
+// console.log('floor_0:', whitelistOutputFloorZero)
+console.log('floor_1:', whitelistOutputFloorOne)
+console.log('floor_2:', whitelistOutputFloorTwo)
+// console.log('whitelistOutputFloorThree:', whitelistOutputFloorThree)
---
+++ multiConditionHandler.js
@@ -0,0 +1,136 @@
+const util = require('util')
+
+// Determine number of conditionals in filename
+function conditionalCount(filename){
+  const conditionals = [' or ', ' and not ', ' and ', ' join ']
+  let count = 0;
+
+  // if(filename.includes(' or ')){ count++; };
+
+  conditionals.forEach(condition => {
+    if(filename.includes(condition)){
+      count++;
+      while (filename.includes(condition)){
+        const idx = filename.indexOf(condition);
+        const beginning = filename.slice(0, idx);
+        const end = filename.slice((idx + condition.length));
+        filename = beginning + " " + end;
+      }
+    };
+  });
+  return count;
+};
+
+// Find index positions for all conditionals
+function findIndexOfEachConditional(filename) {
+  const conditionalsList = [' or ', ' and not ', ' and ', ' join ']
+  let earliestConditionalIdx = null;
+  let output = {
+    firstConditional: null,
+    conditionals: {' or ': [], ' and not ': [], ' and ': [], ' join ': [] }
+  }
+
+  // Iterate through entire filename string
+  for(var fileIdx = 0, fileLength = filename.length; fileIdx < fileLength; fileIdx++){
+    // For each string position - check to see if it is the start of a conditional
+    for(var checkCondition = 0; checkCondition < conditionalsList.length; checkCondition++ ){
+      // Grabs part of string starting with current index and adding to it the length of conditional to check
+      let stringToMatch = filename.slice(fileIdx, fileIdx + conditionalsList[checkCondition].length)
+      if(stringToMatch === conditionalsList[checkCondition]){
+        output.conditionals[conditionalsList[checkCondition]].push(fileIdx)
+      }
+    }
+  }
+
+  // Filter out false positives that occur when both 'and' & 'and not' match - if duplicates remove index from 'and' array
+  output.conditionals[' and '] = output.conditionals[' and '].filter(index => !output.conditionals[' and not '].includes(index))
+
+  // Adds to output object the first conditional index found and the type
+  for(cond in output.conditionals){
+    output.conditionals[cond].forEach(index => {
+      if(!earliestConditionalIdx || index < earliestConditionalIdx){
+        earliestConditionalIdx = index;
+        output.firstConditional = {ruleType: cond, index: index}
+      }
+    })
+  }
+  // console.log('output in findIndexOfEachConditional', output)
+  return output
+};
+
+function sortMultiConditionIntoArrays(objectWithConditionalIndices, filename) {
+  // Store array of all spaces within a filename which will be used to find end of options
+  const spaceIndexes = [];
+  filename.split('').map((char, i) => {
+    if(char === ' '){
+      spaceIndexes.push(i)
+    }
+  })
+
+  // Create object to store options by condition type
+  sortedConditionalHrefs = {
+    conditionalToReplace: objectWithConditionalIndices.firstConditional.ruleType,
+    conditionalLists: {
+      ' or ': [],
+      ' and ': [],
+      ' and not ': [],
+      ' join ': []
+    }
+  }
+
+  // Handle first conditional
+  const firstCondIndex = objectWithConditionalIndices.firstConditional.index
+  sortedConditionalHrefs.conditionalLists[objectWithConditionalIndices.firstConditional.ruleType].push(filename.slice(0, firstCondIndex))
+
+  // handle remaining
+  for(conditional in objectWithConditionalIndices.conditionals){
+    objectWithConditionalIndices.conditionals[conditional].map(index => {
+      const endSlice = spaceIndexes.find(spaceIdx => spaceIdx > (index + conditional.length))
+      const startSlice = index + conditional.length
+      const slicedOption = filename.slice(startSlice, endSlice)
+      sortedConditionalHrefs.conditionalLists[conditional].push(slicedOption)
+
+    })
+  }
+  // console.log('sortedConditionalHrefs in sortMultiConditionIntoArrays', sortedConditionalHrefs)
+  return sortedConditionalHrefs
+}
+
+function formatMultiCustomRule(result, floorNum, originalFilename){
+  console.log('result:\n', result)
+  const output = {
+    altHref: originalFilename,
+    floor: floorNum,
+    ruleType: 'multi',
+    groupToReplace: null,
+    multiGroup: {}
+  }
+  const conditionalDict = {
+    ' or ': 'multiOr',
+    ' and ': 'multiAnd',
+    ' and not ': 'multiAndNot',
+    ' join ': 'multiJoin'
+  }
+
+  for(condType in result.conditionalLists){
+    if(result.conditionalLists[condType].length > 0){
+      output.multiGroup[conditionalDict[condType]] = result.conditionalLists[condType]
+
+      if(condType === result.conditionalToReplace){
+        output.groupToReplace = conditionalDict[condType]
+      }
+      // output.multiGroup.push({[conditionalDict[condType]] = result.conditionalLists[condType]})
+    }
+  }
+
+  // console.log('\noutput at end of formatMultiCustomRule:\n', util.inspect(output, {showHidden: false, depth: null} ))
+  return output
+}
+
+
+module.exports = {
+  conditionalCount: conditionalCount,
+  findIndexOfEachConditional: findIndexOfEachConditional,
+  sortMultiConditionIntoArrays: sortMultiConditionIntoArrays,
+  formatMultiCustomRule: formatMultiCustomRule
+}
